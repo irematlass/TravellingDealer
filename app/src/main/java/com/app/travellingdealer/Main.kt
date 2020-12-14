3d142a66db1dc582638ec0c724a44ea62ca75746
irem atlas <irem.atlas@vinya.com.tr>
2020-12-14 16:52:19 +0300

    completed algorithm
    add delete func and test

--- app/src/main/java/com/app/travellingdealer/Main.kt
+++ app/src/main/java/com/app/travellingdealer/Main.kt
@@ -1,12 +1,23 @@
 package com.app.travellingdealer
 
-class Main {
-    fun main(args: Array<String>) {
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         // Example
-        /*val node = Node(4)
-        node.appendToEnd(5)
-        node.appendToEnd(6)
-        node.appendToEnd(7)*/
-        // End of Example
+        val node = Node(1,4)
+        node.appendToEnd(6,5)
+        node.appendToEnd(6,2)
+        node.appendToEnd(8,9)
+        node.printNodes()
+        Log.d("dealerLenght",node.length(node).toString())
+        Log.d("dealerSum",node.sumOfNodes().toString())
+        node.deleteNode(node,6,5)
+        node.printNodes()
+
     }
 }
--- app/src/main/java/com/app/travellingdealer/Node.kt
+++ app/src/main/java/com/app/travellingdealer/Node.kt
@@ -1,21 +1,22 @@
 package com.app.travellingdealer
 
+import android.util.Log
+
 class Node(var x:Int, var y:Int) {
 
 
     var following: Node? = null
-    var head: Node? = null
+    var head: Node? = this
     fun appendToEnd(x:Int,y:Int) {
 
         val end = Node(x,y)
         var n: Node? = this
-        if(head == null) head = Node(x,y)
-        else {
+
             while (n!!.following != null) {
                 n = n.following
             }
             n.following = end
-        }
+
     }
 
     // TODO:: Implement to return the length of the SinglyLinkedList
@@ -23,7 +24,7 @@ class Node(var x:Int, var y:Int) {
     fun printNodes() {
         var cur = head
         while(cur != null) {
-            print("${cur.x},${cur.y} ")
+            Log.d("dealer1","${cur.x},${cur.y} ")
             cur = cur?.following
         }
     }
@@ -50,25 +51,31 @@ class Node(var x:Int, var y:Int) {
         return result
     }
 
-    fun deleteNode(head: Node, x: Int, y: Int): Node? {
+    fun deleteNode(head: Node, x: Int, y: Int) :Node?{
         val n: Node = head
-
-        if(n.x==x && n.y==y) {
+        var tempNode: Node? = head
+        var prevNode: Node? = null
+        if(tempNode != null && n.x==x && n.y==y) {
             return head.following
         }
         // TODO:: Implement the proper loop in order to remove given data
-           while(!(n.x==x && n.y==y)){
-              
-           }
-        //
-        return head
+        while (tempNode != null && !(tempNode.x == x && tempNode.y == y)) {
+            prevNode = tempNode
+            tempNode = tempNode.following
+        }
+        if (tempNode == null)
+            return head
+        prevNode?.following =tempNode?.following
+
+          return tempNode
+
     }
     fun calculateDistance(x:Int,y:Int):Int{
-       var firstVal=Math.pow(x-3.toDouble(),2.0)
+        var firstVal=Math.pow(x-3.toDouble(),2.0)
         var secondVal=Math.pow(y-7.toDouble(),2.0)
+        var distance=Math.sqrt(firstVal+secondVal).toInt()
 
-
-     return Math.sqrt(firstVal+secondVal).toInt()
+     return distance*2
     }
 }
 
